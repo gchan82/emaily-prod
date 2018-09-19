36018dfd7a43b96a0fa432979332c736f25c5e77
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-09-18 17:16:16 -0700

    update

--- config/keys.js
+++ config/keys.js
@@ -1,15 +1,9 @@
 // keys.js dev or prod?
 if (process.env.NODE_ENV === 'production') {
-  //we are in prod - rtn the prod set of keys
+  //if we are in prod - rtn the prod set of keys
   module.exports = require('./prod');
 } else {
-  //we are in dev - rtn dev keys!!!
+  //if we are in dev - rtn dev keys!!!
   module.exports = require('./dev');
 }
 
-module.exports = {
-  googleClientID: '526206735091-in3vp134trqi1m58hvuqeh8jl4q1i2gn.apps.googleusercontent.com',
-  googleClientSecret: 'vRLPQNQYYvxC70PYokxloyqB',
-  mongoURI: 'mongodb://gary:pass123@ds147942.mlab.com:47942/emaily-dev',
-  cookieKey: 'cookiekeyrandomletterssdgiaohapdhnalkaysgiandjolghlkdfhagi'
-};
---
+++ gitignore/config.keys.backup.js
@@ -0,0 +1,8 @@
+// info b4 heroku setup
+
+module.exports = {
+  googleClientID: '526206735091-in3vp134trqi1m58hvuqeh8jl4q1i2gn.apps.googleusercontent.com',
+  googleClientSecret: 'vRLPQNQYYvxC70PYokxloyqB',
+  mongoURI: 'mongodb://gary:pass123@ds147942.mlab.com:47942/emaily-dev',
+  cookieKey: 'cookiekeyrandomletterssdgiaohapdhnalkaysgiandjolghlkdfhagi'
+};
